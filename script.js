b3862f8f47c0f2584413d395b338f8ac610c1a6c
Swapna Pervin <swapnapervin08@gmail.com>
2026-08-05 21:10:39 +0600

    Update portfolio website

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("JavaScript Connected Successfully!");
+let hireButton = document.getElementById("hireBtn");
+
+hireButton.addEventListener("click", function () {
+    alert("Thank you for visiting my Portfolio!");
+
+let title = document.getElementById("title");
+
+title.innerHTML = "Welcome to My Portfolio"; title.style.color = "red";
+
+
+});
